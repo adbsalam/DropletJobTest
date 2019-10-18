dcaeee4a0104c1f46416b1611c5625b55a86b584
Muhammad <abdulsalamch32@gmail.com>
2019-10-18 21:51:26 +0100

    Used C++ library in existing project
    Added c++ library to return hello world!
    Implimented in existing project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,7 +6,7 @@
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_salam_dropletjobtest_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
-    
+
 
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
